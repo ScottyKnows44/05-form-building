a407936b7096865ce84ae8c31f89b0fddf319b8a
ScottyKnows44 <Scottcfalcon@gmail.com>
2018-03-18 11:08:17 -0700

    finished with lab

--- scott/scripts/articleView.js
+++ scott/scripts/articleView.js
@@ -22,8 +22,7 @@ articleView.populateFilters = () => {
 };
 
 articleView.handleAuthorFilter = () => {
-  $('#author-filter').on('change', function(e) {
-    e.preventDefault();
+  $('#author-filter').on('change', function() {
     if ($(this).val()) {
       $('article').hide();
       $(`article[data-author="${$(this).val()}"]`).fadeIn();
@@ -75,55 +74,60 @@ articleView.setTeasers = () => {
 };
 
 // COMMENT: Where is this function called? Why?
-// This Function is called in the new.html page 
-articleView.initNewArticlePage = function() {
-  // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later.
+// PUT YOUR RESPONSE HERE
+articleView.initNewArticlePage = () => {
+  // DONE: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
+
   $('.tab-content').show();
 
   // TODO: The new articles we create will be copy/pasted into our source data file.
   // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
+
   $('#export-field').hide();
+
+  // This will, when you click on the input with then JSON in it, will select all the text in the input.
   $('#article-json').on('focus', function(){
     this.select();
   });
 
-  // TODO: Add an event handler to update the preview and the export field if any inputs change.
-  $('#new-form').on('change', 'input, textarea', articleView.create);
+  // DONE: Add an event handler to update the preview and the export field if any inputs change.
+
+  $('form').on('change', articleView.create);
+
 };
 
 articleView.create = () => {
-  // TODO: Set up a variable to hold the new article we are creating.
+  // DONE: Set up a variable to hold the new article we are creating.
   // Clear out the #articles element, so we can put in the updated preview
-  let article;
-  $('#articles').empty();
+  $('#artices').empty();
 
-  // TODO: Instantiate an article based on what's in the form fields:
-  article = new Article({
+  // DONE: Instantiate an article based on what's in the form fields:
+  let article = new Article({
     title: $('#article-title').val(),
     author: $('#article-author').val(),
-    authorUrl: $('#article-author-url').val(),
+    authorUrl: $('#article-author-Url').val(),
     category: $('#article-category').val(),
-    body: $('#article-body').val(),
-    publishedOn: $('#article-published:checked').length ? new Date() : null
+    publishedOn: $('#article-published').val(),
+    body: $('#article-body').val()
   });
 
-  // TODO: Use our interface to the Handblebars template to put this new article into the DOM:
+
+  // DONE: Use our interface to the Handblebars template to put this new article into the DOM:
+
   $('#articles').append(article.toHtml());
 
-  // TODO: Activate the highlighting of any code blocks:
-  $('pre code').each(function(i, block) {
-    hljs.highlightBlock(block);
-  });
+  // TODO: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
+  $('pre code').each('stuff goes in here');
 
-  // TODO: Export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
+  // TODO: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
   $('#export-field').show();
-  $('#article-json').val(JSON.stringify(article) + ',');
+  $('#article-json').val(JSON.stringify(article))
 };
 
 // COMMENT: Where is this function called? Why?
-// articles.forEach goes here so it can render all the articles and display them
+// PUT YOUR RESPONSE HERE
 articleView.initIndexPage = () => {
-  articles.forEach(article => $('#articles').append(article.toHtml()));
+  article.forEach(article => $('#articles').append(article.toHtml()));
   articleView.populateFilters();
   articleView.handleCategoryFilter();
   articleView.handleAuthorFilter();
